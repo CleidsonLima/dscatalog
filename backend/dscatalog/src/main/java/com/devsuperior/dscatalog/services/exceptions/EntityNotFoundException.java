11356297ad8c22c19eb9e83214607060acac1eb9
CleidsonLima <cleidson_lima@outlook.com>
2025-04-14 11:50:59 -0300

    Find Category by id, exception handling

---
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    private static final long SerialVersionUID = 1L;
+    public EntityNotFoundException( String msg){
+        super(msg);
+    }
+}
